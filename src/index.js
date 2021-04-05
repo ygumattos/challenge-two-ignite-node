52144b37727abf34dc2fa758ae77dc5cf694ac83
tma00436 <tma00436@sky.com.br>
2021-04-05 15:32:01 -0300

    finish challange

--- src/index.js
+++ src/index.js
@@ -10,19 +10,55 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const foundUser = users.find(user => user.username === username)
+
+  if(!foundUser) return response.status(404).json({ error: 'User not found'})
+
+  request.user = foundUser
+  
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user.pro && user.todos.length === 10) return response.status(403).json('You need update your plan')
+  
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) return response.status(404).json({ error: 'User not found'})
+
+  if(!validate(id)) return response.status(400).json({ error: 'Invalid ID'})
+
+  const foundTodo = user.todos.find(todo => todo.id === id)
+
+  if(!foundTodo) return response.status(404).json({ error: 'Todo not found'})
+
+  request.user = user
+  request.todo = foundTodo
+
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const foundUser = users.find(user => user.id === id)
+
+  if(!foundUser) return response.status(404).json({ error: 'User not found'})
+
+  request.user = foundUser
+  
+  next()
 }
 
 app.post('/users', (request, response) => {
